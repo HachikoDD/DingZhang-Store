db6d8bb4ecd31f31e451ad0801becac5e6c21b76
HachikoDD <zdmwj123@gmail.com>
2021-12-23 10:01:06 +0800

    DZ

--- scripts/start.js
+++ scripts/start.js
@@ -6,7 +6,7 @@ const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_module
 concurrently([
     { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     { 
-        command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
+        command: `"${browserSyncPath}" start --proxy http://127.0.0.1 --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
         name: 'SB_BROWSER_SYNC', 
         prefixColor: 'bgGreen.bold',
     }
